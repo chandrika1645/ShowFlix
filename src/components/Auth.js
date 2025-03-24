e5d394cf0a328acfd15d4d647536e29fda522fa8
Chandrika Sharma <chandrikasharma@Chandrikas-MacBook-Air.local>
2025-03-24 12:26:09 +0530

    change firebase auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGBPDzpLOTxktW6SrAXs00iDiVAxQhcXc",
-  authDomain: "disney-hotstar-clone-8241d.firebaseapp.com",
-  projectId: "disney-hotstar-clone-8241d",
-  storageBucket: "disney-hotstar-clone-8241d.firebasestorage.app",
-  messagingSenderId: "324590947563",
-  appId: "1:324590947563:web:23a37b2330dd52f83a42ae",
-  measurementId: "G-J69F3F3BJ5",
+  apiKey: "AIzaSyAB54Ze8-8CRwvJts2o9dnf2z4Vca9SFcU",
+  authDomain: "show-flix-f0c2c.firebaseapp.com",
+  projectId: "show-flix-f0c2c",
+  storageBucket: "show-flix-f0c2c.firebasestorage.app",
+  messagingSenderId: "347442870553",
+  appId: "1:347442870553:web:14e444e99268b538426d88",
+  measurementId: "G-6LR2PSBRSB"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,13 +5,12 @@ const Login = (props) => {
     <Container>
       <Content>
         <CTA>
-          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+        <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
           <Description>
             Get Premier Access to Raya and the Last Dragon for an additional fee
             with a Disney+ subscription. As of 03/26/21, the price of Disney+
             and The Disney Bundle will increase by $1.
           </Description>
-          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
         </CTA>
         <BgImage />
       </Content>
